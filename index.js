690ff39fde37eb5e1f2bb7b17e4223aca58f3867
Xiaochen Xu <xiaochex@gmail.com>
2021-02-21 15:42:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",];
+
+function destructivelyAppendCat(){
+     cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    return appendCat=[...cats,'Broom'];
+}
+
+function prependCat(){
+    return prependCat=['Arnold',...cats,];
+}
+
+function removeLastCat(){
+    return removeLastCat = cats.slice(0,cats.length - 1);
+}
+
+function removeFirstCat(){
+    return removeFirstCat = cats.slice(1);
+}
